fd6f04771ab1bed0fedec924989c4c28a2516469
Your Name <you@example.com>
2023-11-16 15:41:11 +0530

    1st commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+let topButton = document.getElementById("backtop");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        topButton.style.display = "flex";
+    }
+    else {
+        topButton.style.display = "none";
+    }
+});
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
